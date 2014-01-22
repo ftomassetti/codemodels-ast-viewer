e69991ef2542b9690d567d594194bbcddfa4b50a
Federico Tomassetti <f.tomassetti@gmail.com>
2014-01-22 23:24:02 +0100

    wip

--- lib/codemodels/ast/viewer.rb
+++ lib/codemodels/ast/viewer.rb
@@ -1,9 +1,43 @@
 require "codemodels/ast/viewer/version"
 
+require 'java'
+
+import java.awt.EventQueue
+import javax.swing.JFrame
+import java.lang.Runnable
+import java.awt.Dimension
+import javax.swing.JSplitPane
+
 module Codemodels
   module Ast
     module Viewer
-      # Your code goes here...
+
+        class EditorFrame < JFrame
+
+            def start
+                self.title = "CodeModels AST-Viewe"
+                self.size = Dimension.new 1000,700
+                self.visible = true
+
+                sp = JSplitPane.new JSplitPane::HORIZONTAL_SPLIT
+                self.content_pane.add sp
+            end
+
+        end
+
+        class AppRunnable
+          java_implements java.lang.Runnable
+          java_signature 'void run()'
+          def run
+            p "running now."
+            p "done running."
+          end
+        end
+
+      
+        r = AppRunnable.new
+
+        EventQueue.invokeLater(r)
     end
   end
 end
