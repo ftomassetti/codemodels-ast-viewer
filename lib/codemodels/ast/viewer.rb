436de3e4a84ebbbe8092c33b9112da59ec2d319d
Federico Tomassetti <f.tomassetti@gmail.com>
2014-02-14 12:29:23 +0100

    Using JGraphX

--- lib/codemodels/ast/viewer.rb
+++ lib/codemodels/ast/viewer.rb
@@ -2,12 +2,21 @@ require "codemodels/ast/viewer/version"
 
 require 'java'
 
+require 'jars/jgraphx.jar'
+
 import java.awt.EventQueue
 import javax.swing.JFrame
 import java.lang.Runnable
 import java.awt.Dimension
 import javax.swing.JSplitPane
 import javax.swing.JTextArea
+import java.awt.Font
+import java.awt.Color
+#import com.mxgraph.swing.mxGraphComponent
+#import com.mxgraph.view.mxGraph
+
+include_class('com.mxgraph.swing.mxGraphComponent') {'MxGraphComponent'}
+include_class('com.mxgraph.view.mxGraph') {'MxGraph'}
 
 module Codemodels
   module Ast
@@ -21,10 +30,37 @@ module Codemodels
                 self.visible = true
 
                 sp = JSplitPane.new JSplitPane::HORIZONTAL_SPLIT
+                sp.setDividerLocation(0.5)
                 self.content_pane.add sp
 
                 ta = JTextArea.new
+                ta.text = "Hello World!"
+                font = Font.new("Verdana", Font::BOLD, 14)
+                ta.setFont(font)
+                ta.setForeground(Color::WHITE)
+                ta.setBackground(Color::BLACK)
+
                 sp.add ta
+                sp.add(create_graph)
+            end
+
+            private
+
+            def create_graph
+              graph = MxGraph.new
+              parent = graph.getDefaultParent
+
+              graph.getModel.beginUpdate
+
+              v1 = graph.insertVertex(parent, nil, "Hello", 20, 20, 80,
+              30)
+              v2 = graph.insertVertex(parent, nil, "World!", 240, 150,
+              80, 30)
+              graph.insertEdge(parent, nil, "Edge", v1, v2)
+              graph.getModel.endUpdate
+
+              graphComponent = MxGraphComponent.new(graph);
+              graphComponent  
             end
 
         end
